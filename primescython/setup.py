3fac600e613b9bdc97acee88ec3fccfab69a0937
mar.salvalaggio@gmail.com <55287795+Salva32@users.noreply.github.com>
2023-03-29 18:43:52 +0200

    test setup

--- primescython/setup.py
+++ primescython/setup.py
@@ -1,11 +1,21 @@
 from setuptools import setup, Extension
 from Cython.Build import cythonize
+from sys import platform
 
 extensions = [
     Extension("primes", ["../primescython/primes.pyx"])
 ]
 
-setup(
-    name="primes",
-    ext_modules=cythonize(extensions)
-)
+if platform == "darwin":
+    setup(
+        name="primes-mac",
+        ext_modules=cythonize(extensions)
+    )
+else:
+    setup(
+        name="primes-mac",
+        ext_modules=cythonize(extensions)
+    )
+
+
+
